dfc8116dfd334e2c4e6a0dc9012a6025f13e90c8
scfmsergio <scfmsergio@gmail.com>
2023-04-25 03:02:49 +0200

    toca

--- MAIN.m
+++ MAIN.m
@@ -1,3 +1,5 @@
+clc; clear all; close all;
+
 %% HELICOPTERS LAB 2
 
 
@@ -19,3 +21,72 @@ geom.deltaZ=1.5;  %[m]
 
 inert_y = geom.m_blade*geom.R^2/4 + geom.m_blade*geom.R^2/12;
 geom.I_y=inert_y;
+
+f = 2.787;
+
+%% Apply bisection method
+
+% Estimate initial guesses
+
+theta_0m = -15;
+theta_0p = 15;
+
+% Enter Trim module
+
+[alpha_R, alpha_d, T_Dtrim, beta_c, beta_s] = mod_trim(flight, geom, f);
+
+% Enter Blade Dynamics module (preliminar calcs of initial guesses in the bisection method)
+
+[theta_sm , theta_cm , beta_0m] = mod_BD (alpha_R , beta_c , beta_s, theta_0m , flight , geom); % Calculate Blade Dynamics for minimum estimated twist
+[theta_sp , theta_cp , beta_0p] = mod_BD (alpha_R , beta_c , beta_s, theta_0p , flight , geom); % Calculate Blade Dynamics for maximum estimated twist
+
+% Enter Aerodynamics module (preliminar calcs of initial guesses in the bisection method)
+
+T_Daero_m = mod_aero (theta_0m , theta_sm , flight , geom);
+T_Daero_p = mod_aero (theta_0p , theta_sp , flight , geom);
+
+errm = T_Daero_m - T_Dtrim;
+errp = T_Daero_p - T_Dtrim;
+
+if errm*errp > 0 
+    
+    error('Change your initial guesses.')
+
+end
+
+% Iteration
+
+for iter = 1:maxiter
+
+    theta_0 = (theta_0m + theta_0p) / 2;
+    
+    [theta_s , theta_c , beta_0] = mod_BD (alpha_R , beta_c , beta_s, theta_0 , flight , geom); % Enter BD module in the iteration
+    T_D = mod_aero (theta_0 , theta_s , flight , geom); % Enter Aero module in the iteration
+
+    err = T_D - T_Dtrim;
+
+    if (abs(err) < tol)
+
+        break
+
+    end
+
+    if err > 0
+
+        theta_0p = theta_0;
+        errp = err;
+
+    else
+
+        theta_0m = theta_0;
+        errm = err;
+
+    end
+
+end
+
+if iter == maxiter
+
+    error('No convergence!')
+
+end
